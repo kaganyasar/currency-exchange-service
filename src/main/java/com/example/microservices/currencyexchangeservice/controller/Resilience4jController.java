14252aca261d226ac12fc8debae88633496f72bd
kaganyasar <o.kagan_yasar@hotmail.com>
2021-04-03 10:24:52 +0300

    "microservices"

--- src/main/java/com/example/microservices/currencyexchangeservice/controller/Resilience4jController.java
+++ src/main/java/com/example/microservices/currencyexchangeservice/controller/Resilience4jController.java
@@ -1,6 +1,8 @@
 package com.example.microservices.currencyexchangeservice.controller;
 
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,12 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
-public class CircuitBreakerController {
+public class Resilience4jController {
 
-    private Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
+    private Logger logger = LoggerFactory.getLogger(Resilience4jController.class);
 
     @GetMapping("/sample-api-retry")
-    @Retry(name = "sample-api-retry", fallbackMethod = "hardCodedResponse")
+    @Retry(name = "sample-retry", fallbackMethod = "hardCodedResponse")
     public String sampleApiRetry() {
         logger.info("Sample Api retry call received");
         ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/some-dummy-url", String.class);
@@ -23,11 +25,25 @@ public class CircuitBreakerController {
     }
 
     @GetMapping("/sample-api-circuit-breaker")
-    @CircuitBreaker(name = "default", fallbackMethod = "hardCodedResponse")
+    @CircuitBreaker(name = "sample-circuit-breaker", fallbackMethod = "hardCodedResponse")
     public String sampleApiCircuitBreaker() {
         logger.info("Sample Api circuit breaker call received");
         ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/some-dummy-url", String.class);
         return forEntity.getBody();
+    }
+
+    @GetMapping("/sample-api-rate-limiter")
+    @RateLimiter(name = "sample-rate-limiter")
+    public String sampleApiRateLimiter() {
+        logger.info("Sample Api rate limiter call received");
+        return "sample-api-rate-limiter response";
+    }
+
+    @GetMapping("/sample-api-bulkhead")
+    @Bulkhead(name = "sample-bulkhead")
+    public String sampleApiBulkhead() {
+        logger.info("Sample Api bulkhead call received");
+        return "sample-api-bulkhead response";
     }
 
     public String hardCodedResponse(Exception ex) {
